e6fdc9df21d783fea951ad354c7fc852db38666a
Margaret Sun <hi.margaret.sun@gmail.com>
2022-11-01 10:31:59 -0700

    initial commit

---
+++ validParentheses.js
@@ -0,0 +1,25 @@
+"use strict";
+
+var isValid = function(s) {
+  if (s.length <= 1) {
+      return false
+  }
+  let arr = []
+  for (let p of s){
+      if (p === "{" || p === "(" || p === "["){
+          arr.push(p)
+      } else if (p === "}" && arr[arr.length-1] === "{"){
+          arr.pop()
+      } else if (p === ")" && arr[arr.length-1] === "("){
+          arr.pop()
+      } else if(p === "]" && arr[arr.length-1] === "["){
+          arr.pop()
+      } else{
+          return false
+      }
+  }
+  if (arr.length){
+    return false
+  }
+  return true
+};
